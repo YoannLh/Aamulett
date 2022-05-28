cd1639efd1a3acb98f29a8d7ef491cd2fd05f048
L'hostis <yolhostis@live.fr>
2022-05-28 23:33:21 +0200

    React Router + Home + Board + Error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+
+import Home from './pages/Home'
+import Board from './pages/Board'
+import Error from './pages/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <div>Go</div>
-    </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/board" element={<Board />} />
+            <Route path="*" element={<Error />} />
+        </Routes>
+    </BrowserRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
